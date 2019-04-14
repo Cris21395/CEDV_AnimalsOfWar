5b1244b693344e7a18fa6770f17bcc95ccbb5f3c
Enrique Garrido Pozo <enrique.garrido@alu.uclm.es>
2019-04-14 23:06:11 +0200

    GameInstance created

---
+++ Source/AnimalsOfWar/AnimalsOfWarGameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "AnimalsOfWarGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMALSOFWAR_API UAnimalsOfWarGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
